6f30624b668622e9cd91feb8a75904aabc92d189
wikivitas <wikivitas@gmail.com>
2020-10-03 13:21:53 +0300

    3. majasdarba iesniegšana/Iepazīšanas ar C++ valodu/Bash script darbošanai ar Git labojums

--- git.sh
+++ git.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
+str AW="D"
 
 clear
 echo "Hi! I'm a Git commit master!"
-sleep 1;echo " ";echo "What do you want?"
-echo "(D - download repository, U - upload repository, C - cancel, sorry and exit)"
-read -p "Your answer: " AW
-sleep 1
-if [ "$AW" = U ]; then
-#to enter email
-read -p "Enter your e-mail: " EM
-git config --global user.email $EM
+sleep 1;
+echo " ";echo "What do you want?"
+while [ "$AW" != C ]; do
+	echo "(D - download repository, U - upload repository, C - cancel, sorry and exit)"
+	i=0;read -p "Your answer: " AW
+	sleep 1
 
-#to enter login
-read -p "Enter your login: " LG
-git config --global user.name $LG
+	if [ "$AW" = D ]; then
+		read -p "Enter your Git repository link: https://github.com/" LNK
+		LNK1="https://github.com/"$LNK
+		git clone $LNK1
+		i=1
+	fi
 
-#to do a tracking of done commit
-git add .
-
-#to give a name of new commit
-read -p "Enter a commit name: " CN
-git commit -m "$CN"
-
-#to init the repository uploading
-git push origin master
-echo "Your commit was succesfully uploaded!"
-fi
-
+	if [ "$AW" = U ]; then
+		read -p "Enter your e-mail: " EM
+		git config --global user.email $EM
+		read -p "Enter your login: " LG
+		git config --global user.name $LG
+		git add .
+		read -p "Enter a commit name: " CN
+		git commit -m "$CN"
+		git push origin master
+		i=1
+	fi
+	if [ "$AW" = C ]; then
+		i=1
+	fi
+	if [ "$i" = 0 ]; then
+		echo "Sorry, but I didn't understand you! Try to answer again! :)"
+	fi
+done
+cd ..
 echo "Good bye! See you soon! :)"
-
-if [ "$AW" = C ]; then
 exit
-fi
