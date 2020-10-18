1dacaf975acf862cf692a6ae2d28182bc92048ce
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-18 15:28:51 +0300

    Create pointers.cpp

---
+++ C++/pointers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+ 
+int main()
+{
+    int a = 7;
+    std::cout << a << '\n'; // выводим значение переменной a
+    std::cout << &a << '\n'; // выводим адрес памяти переменной a
+ 
+    return 0;
+}
