686b02b694b63d0bdc5cafcd15fba884c4a964e1
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-19 11:55:36 +0300

    PU1_un_PU2_uzdevumu_izpilde

--- PU/PU2.c
+++ PU/PU2.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 
 int main(){
-	char a,i=0;
-	char b[8]; //skaitļu zīmes masīvs
-	printf("Ievadiet decimālo skaitļu: ");scanf("%d",&a);
-	printf("Binārais skaitlis: ");
+	char a,i=0; //a mainīgai ir char tips
+	const char n=8; //izvadīto zīmes skaits
+	char b[n]; //skaitļu zīmes masīvs
+	printf("Ievadiet decimalo skaitlu: ");scanf("%d",&a);
+	printf("Binarais skaitlis: ");
 	do{
-		i++;b[i]=a%2;
-		a=a>>1;
+		i++;b[i]=a%2;a=a>>1;
 	}while(a!=0);
+	while(i!=n){ 
+		i++;b[i]=0;
+	}
 	do{
 		printf("%d",b[i]);i--;
 	}while(i!=0);
