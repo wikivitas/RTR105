e181ecf8ddcdf28d78ac063ad2bf167d9080315c
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-19 19:57:21 +0300

    PU1_un_PU2_uzdevumu_izpilde

--- PU/PU2.c
+++ PU/PU2.c
@@ -4,7 +4,7 @@
 int main(){
 	char a; //a mainīgai ir char tips
 	char i=0; //skaitītāja mainīga
-	const char n=4; //izvadīto zīmes skaits
+	const char n=8; //izvadīto zīmes skaits (char tipam parasti ņēm 8 zīmes, jo char tipu mainīgo izmērs ir 1 b)
 	char b[n+1]; //skaitļu zīmes masīvs
 
 	printf("Ievadiet decimalo skaitlu: ");scanf("%d",&a);
@@ -16,6 +16,7 @@ int main(){
 	//ja sanāks, ka zīmju nepietiek, parādīsies bridinājums, un programa tiks pārtraukta ar 0 atgriešanu
 	if(i>n){ printf("Kļūda! Bināro skaitli nevar attēlot ar norādīto zīmju skaitu vērtību!\n");exit(0);} 
 
+	//piešķirot nepieprasītiem augstas kārtas pozicijam 0 vērtību
 	while(i!=n){ 
 		i++;b[i]=0;
 	}
