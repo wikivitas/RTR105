ca0d668f619f5bae59ee6a84416783d191c9bf1d
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-29 21:45:09 +0200

    Create user_function.cpp

---
+++ C++/user_function.cpp
@@ -0,0 +1,22 @@
+//a code example from https://www.geeksforgeeks.org/functions-in-c/
+
+#include <iostream> 
+using namespace std; 
+  
+int max(int x, int y)  
+{  
+    if (x > y)  
+    return x;  
+    else
+    return y;  
+}  
+  
+int main() { 
+    int a = 10, b = 20;  
+  
+    // Calling above function to find max of 'a' and 'b'  
+    int m = max(a, b);  
+  
+    cout << "m is " << m;  
+    return 0;  
+} 
