ce38e8852511689bdf524976fde37eae539a1a03
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-31 12:44:38 +0200

    Update PU3.c

--- PU/PU3.c
+++ PU/PU3.c
@@ -11,12 +11,15 @@ int main(){
 void sort(int a, int b, int c, char s){
   printf("Kārtošanas secība(A - augoša, D - dilstoša): ");
   char s; scanf("%c",s);
-  if(){
+  if(s==65){
+	  printf("Ievadiet, lūdzu, 1. skaitļu: ");scanf("%d",&a);
   }
-  if(){
+  
+  if(s==68){
   
   }
-  else(
-  
+  else{
+  	printf("Nav izvelēta kartošanas secība!\n\n");
+  goto A;
   }
 }
