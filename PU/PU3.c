28e037fbb1b0b692fe60266ad812c0eed17d7ff0
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-31 16:14:06 +0200

    PU3_uzdevumu_izpilde

--- PU/PU3.c
+++ PU/PU3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 void sort(int, int, int, char);
-void swap(int &a, int &b){
-	int temp=a;
-	a=b;
-	b=temp;
+void swap(int *a, int *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
 }
 
 int main(){
