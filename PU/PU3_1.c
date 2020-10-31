dedd7813cfac9a561242b72b10f344a4ac962643
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-31 16:30:13 +0200

    Update PU3_1.c

--- PU/PU3_1.c
+++ PU/PU3_1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 void sort(char, char, char, char);
-void swap(char &a, char &b){
-	char temp=a;
-	a=b;
-	b=temp;
+void swap(char *a, char *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
 }
 
 int main(){
   char a,b,c,s;
   printf("Kartošanas secība(A - augoša, D - dilstoša): "); scanf("%s",&s);
-  printf("Ievadiet, lūdzu, 1. burtu: ");scanf("%s",&a);
-  printf("Ievadiet, lūdzu, 2. burtu: ");scanf("%s",&b);
-  printf("Ievadiet, lūdzu, 3. burtu: ");scanf("%s",&c);
+  printf("Ievadiet, lūdzu, 1. burtu: ");scanf("%d",&a);
+  printf("Ievadiet, lūdzu, 2. burtu: ");scanf("%d",&b);
+  printf("Ievadiet, lūdzu, 3. burtu: ");scanf("%d",&c);
   sort(a,b,c,s);
   return 0;
 }
@@ -22,10 +22,10 @@ void sort(char a, char b, char c, char s){
   if(s==65){
 	while( ((a<b) && (b<c))!=1 ){
 		if(a>b){
-			swap(a,b);
+			swap(&a,&b);
 		}
 		if(b>c){
-			swap(b,c);
+			swap(&b,&c);
 		}
 	}
   }
@@ -33,15 +33,15 @@ void sort(char a, char b, char c, char s){
   if(s==68){
  	while( ((a>b) && (b>c))!=1 ){
 		if(a<b){
-			swap(a,b);
+			swap(&a,&b);
 		}
 		if(b<c){
-			swap(b,c);
+			swap(&b,&c);
 		}
 	}
   }
   if((s!=65)&&(s!=68)){
-  	printf("Nav izvelēta kartošanas secība!\n\n");
+  	printf("Nav izvelēta kartošanas secība! Kārtošana netiks izpildīta!\n\n");
   }
-  printf("Burtu sakārtojums:\c%d\c%d\c%d\n",a,b,c);
+  printf("Burtu sakārtojums:\n%c\n%c\n%c\n",a,b,c);
 }
