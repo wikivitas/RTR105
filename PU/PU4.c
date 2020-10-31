ec2a237d7542d52eedd61f8402f0bc3f50ae41c3
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-31 22:44:46 +0200

    PU4_uzdevumu_izpilde

--- PU/PU4.c
+++ PU/PU4.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
-
-  return 0;
+    char type[9],i;
+    printf("Ievadiet datu tipu: ");gets(type);
+    char ln=strlen(type);
+    if(ln==4) i=1;
+    if(ln==3) i=2;
+    if(ln==9) {i=4;}
+    if(ln!=3 && ln!=4 && ln!=9){printf("Atvainojiet! Ar šo datu tipu nevar stradāt!\n");exit(0);};
+    int j=(int *) malloc(i);
+    printf("Ievadiet veselu skaitļu: ");scanf("%d",&j);
 }
