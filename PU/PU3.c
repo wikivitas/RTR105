f5eeebc910b8e98ff1acc5952e65ec98b4f55d9d
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-10-31 14:32:12 +0200

    Update PU3.c

--- PU/PU3.c
+++ PU/PU3.c
@@ -1,50 +1,47 @@
 #include <stdio.h>
 
 void sort(int, int, int, char);
-int swap(int, int);
+void swap(int &a, int &b){
+	int temp=a;
+	a=b;
+	b=temp;
+}
 
 int main(){
   int a,b,c; char s;
+  printf("Kartošanas secība(A - augoša, D - dilstoša): "); scanf("%s",&s);
+  printf("Ievadiet, lūdzu, 1. skaitļu: ");scanf("%d",&a);
+  printf("Ievadiet, lūdzu, 2. skaitļu: ");scanf("%d",&b);
+  printf("Ievadiet, lūdzu, 3. skaitļu: ");scanf("%d",&c);
   sort(a,b,c,s);
   return 0;
 }
 
-int swap(int a, int b){
-	int temp=a;
-	a=b; 
-	b=temp;
-}
-
 void sort(int a, int b, int c, char s){
-  printf("Kārtošanas secība(A - augoša, D - dilstoša): ");
-  char s; scanf("%c",s);
-  printf("Ievadiet, lūdzu, 1. skaitļu: ");scanf("%d",&a);
-  printf("Ievadiet, lūdzu, 2. skaitļu: ");scanf("%d",&b);
-  printf("Ievadiet, lūdzu, 3. skaitļu: ");scanf("%d",&c);
+  
   if(s==65){
-	while((a<b) && (b<c)){
+	while( ((a<b) && (b<c))!=1 ){
 		if(a>b){
-			swap(a,b);printf("%d %d\n",a,b);
+			swap(a,b);
 		}
 		if(b>c){
-			swap(b,c);printf("%d %d\n",a,b);
+			swap(b,c);
 		}
 	}
   }
   
   if(s==68){
- 	while((a>b) && (b>c)){
+ 	while( ((a>b) && (b>c))!=1 ){
 		if(a<b){
-			swap(a,b);printf("%d %d\n",a,b);
+			swap(a,b);
 		}
 		if(b<c){
-			swap(b,c);printf("%d %d\n",a,b);
+			swap(b,c);
 		}
 	}
   }
   if((s!=65)&&(s!=68)){
-  	printf("Nav izveleta kartosanas seciba!\n\n");
-  goto A;
+  	printf("Nav izvelēta kartošanas secība!\n\n");
   }
-  printf("Skaitlu sakartojums:\n%d\n%d\n%d\n",a,b,c);
+  printf("Skaitļu sakārtojums:\n%d\n%d\n%d\n",a,b,c);
 }
