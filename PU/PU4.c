ec308c52e4beeffa33dc6a014d1a0af52905a6af
wikivitas <wikivitas@gmail.com>
2020-11-04 18:46:03 +0200

    PU4_uzdevuma_izpilde

--- PU/PU4.c
+++ PU/PU4.c
@@ -2,14 +2,60 @@
 #include <stdlib.h>
 #include <string.h>
 
+int cstr(char *, char *);
+long long int fact(unsigned char);
+
 int main(){
-    char type[9],i;
-    printf("Ievadiet datu tipu: ");gets(type);
-    char ln=strlen(type);
-    if(ln==4) i=1;
-    if(ln==3) i=2;
-    if(ln==9) {i=4;}
-    if(ln!=3 && ln!=4 && ln!=9){printf("Atvainojiet! Ar šo datu tipu nevar stradāt!\n");exit(0);};
-    int j=(int *) malloc(i);
-    printf("Ievadiet veselu skaitļu: ");scanf("%d",&j);
+	char type[30];
+	unsigned char i;
+	printf("Ievadiet datu tipu(char,int,long long int): ");gets(type);
+	printf("Ievadiet veselu skaitļu: ");scanf("%hhu",&i);
+	if(i<=0){
+		printf("Skaitlis nevar būt mazāks par 0 vai vienāds ar 0!\n");exit(0);
+	}
+	printf("%hhu! = ",i);
+	unsigned char j=i-1;
+
+	if(cstr(type,"char")==0){
+		char k=i*j;
+		i--;j--;
+		while(j!=1){
+			k*=j;j--;
+		}
+		printf("%hhi\n",k);exit(0);
+	}
+
+	if(cstr(type,"int")==0){
+		int k=i*j;
+		i--;j--;
+		while(j!=1){
+			k*=j;j--;
+		}
+		printf("%d\n",k);exit(0);
+	}
+
+	if(cstr(type,"long long int")==0){
+		long long int k=i*j;
+		i--;j--;
+		while(j!=1){
+			k*=j;j--;
+		}
+		printf("%lld\n",k);exit(0);
+	}
+	else{
+		printf("Nepareizais datu tips! Faktoriāls netiks izreķināts!\n");exit(0);
+	}
+}
+int cstr(char *a, char *b){
+	while(*a==*b){
+		if(*a=='\0' || *b =='\0') break;
+
+	a++;
+	b++;
+	}
+
+	if (*a =='\0' && *b=='\0')
+		return 0;
+	else
+		return -1;
 }
