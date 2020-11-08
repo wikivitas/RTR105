88f503e5bd9e96ad5946b2fe77995d41586ec8b3
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-11-08 14:27:42 +0200

    PU4_uzdevuma_izpilde

--- PU/PU4.c
+++ PU/PU4.c
@@ -2,41 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-int cstr(char *, char *);
+int cstr(char*, char*);
 
 int main(){
 	char type[15]; unsigned char i;
 	printf("Ievadiet datu tipu(char,int,long long): ");gets(type);
 	printf("Ievadiet veselu skaitļu: ");scanf("%hhu",&i);
-	if(i<=0){
+	if((char) i<=0){
 		printf("Skaitlis nevar būt mazāks par 0 vai vienāds ar 0!\n");exit(0);
 	}
-	printf("%hhu! = ",i);
+	printf("%hhu! = ",i); 
 	unsigned char j=i-1;
 
 	if(cstr(type,"char")==0){
 		char k=i*j;
-		i--;j--;
 		while(j!=1){
-			k*=j;j--;
+			j--;k*=j;
 		}
 		printf("%hhi\n",k);exit(0);
 	}
 
 	if(cstr(type,"int")==0){
 		int k=i*j;
-		i--;j--;
 		while(j!=1){
-			k*=j;j--;
+			j--;k*=j;
 		}
 		printf("%d\n",k);exit(0);
 	}
 
 	if(cstr(type,"long long")==0){
 		long long k=i*j;
-		i--;j--;
 		while(j!=1){
-			k*=j;j--;
+			j--;k*=j;
 		}
 		printf("%lld\n",k);exit(0);
 	}
@@ -45,12 +42,10 @@ int main(){
 	}
 	return 0;
 }
-int cstr(char *a, char *b){
+int cstr(char* a, char* b){
 	while(*a==*b){
 		if(*a=='\0' || *b =='\0') break;
-
-	a++;
-	b++;
+	a++;b++;
 	}
 
 	if (*a =='\0' && *b=='\0')
@@ -58,4 +53,3 @@ int cstr(char *a, char *b){
 	else
 		return -1;
 }
-
