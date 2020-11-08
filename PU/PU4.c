07a1d73e4becbb232c443fab2f869c6366b2172d
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-11-09 01:44:23 +0200

    PU4_uzdevuma_izpilde

--- PU/PU4.c
+++ PU/PU4.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-int c(char*, char*);
+long long fact(unsigned char);
+void na(){
+    printf("Nav atbildes!\n");exit(0);
+}
 
 int main(){
-	char type[11]; char brd[]="Nepareizais datu tips! Faktoriāls netiks izreķināts!\n";
-	unsigned char i;
+	char type[11];	unsigned char i;
 	printf("Ievadiet datu tipu(char,int,long long): ");
 	fgets(type,11,stdin);fflush(stdin);
 	printf("Ievadiet veselu skaitļu: ");scanf("%hhu",&i);
@@ -14,47 +16,28 @@ int main(){
 		printf("Skaitlis nevar būt mazāks par 0 vai vienāds ar 0!\n");exit(0);
 	}
 	printf("%hhu! = ",i); 
-	unsigned char j=i-1;
-
-	if(c(type,"char\n")==0){
-		char k=i*j; char a=k;
-		while(j!=1){
-			j--;k*=j;
-			if(k<a){printf("%s",brd);exit(0);}
-		}
-		printf("%hhi\n",k);exit(0);
+	if(strcmp(type,"char\n")==0){
+        	char c=i*(i-1);char k=fact(i);
+        	if(k<c) na(); printf("%hhi\n",k);exit(0);
 	}
 
-	if(c(type,"int\n")==0){
-		int k=i*j;int a=k;
-		while(j!=1){
-			j--;k*=j;
-			if(k<a){printf("%s",brd);exit(0);}
-		}
-		printf("%d\n",k);exit(0);
+	if(strcmp(type,"int\n")==0){
+        	int c=i*(i-1);int k=fact(i);
+        	if(k<c) na(); printf("%d\n",k);exit(0);
 	}
 
-	if(c(type,"long long\n")==0){
-		long long k=i*j;long long a=k;
-		while(j!=1){
-			j--;k*=j;
-			if(k<a){printf("%s",brd);exit(0);}
-		}
-		printf("%lld\n",k);exit(0);
+	if(strcmp(type,"long long\n")==0){
+        	long long c=i*(i-1);long long k=fact(i);
+        	if(k<c) na(); printf("%lld\n",k);exit(0);
 	}
-	else{
-		printf("%s",brd);exit(0);
-	}
+	else na();
 	return 0;
 }
-int c(char* a, char* b){
-	while(*a==*b){
-		if(*a=='\0' || *b =='\0') break;
-	a++;b++;
-	}
 
-	if (*a =='\0' && *b=='\0')
-		return 0;
-	else
-		return -1;
+long long fact(unsigned char i){
+   		unsigned char j=i-1; long long k=i*j; long long a=k;
+		while(j!=1){
+			j--;k*=j;
+		}
+		return k;
 }
