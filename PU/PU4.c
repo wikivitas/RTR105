8129061947305d26468866cd3fcce319bce68167
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-11-10 00:15:13 +0200

    PU4_uzdevumu_izpilde

--- PU/PU4.c
+++ PU/PU4.c
@@ -4,35 +4,36 @@
 
 long long fact(unsigned char i){
    		unsigned char j=i-1; long long k=i*j;
-		while(j!=1){
-			j--;k*=j;
+		while(j--!=1){
+			k*=j;
 		}
 		return k;
 }
+
 void na(unsigned char* i){
     printf("\rNav atbildes! Faktoriālu no skaitļa %hhu nevar aprēķināt!\n",*i);exit(0);
 }
 
 int main(){
-	char type[11];	unsigned char i;
-	printf("Ievadiet datu tipu(char,int,long long): ");
-	fgets(type,11,stdin);fflush(stdin);
+	char t;	unsigned char i;
+	printf("Ievadiet datu tipa burtu(c: char; i: int; l: long long): ");scanf(" %c",&t);
 	printf("Ievadiet veselu skaitļu: ");scanf("%hhu",&i);
 	if((char) i<=0){
 		printf("Skaitlis nevar būt mazāks par 0 vai vienāds ar 0!\n");exit(0);
 	}
 	printf("%hhu! = ",i); 
-	if(strcmp(type,"char\n")==0){
+	
+	if(t=='c'){
         	char c=i*(i-1);char k=fact(i);
         	if(k<c) na(&i); printf("%hhi\n",k);exit(0);
 	}
 
-	if(strcmp(type,"int\n")==0){
+	if(t=='i'){
         	int c=i*(i-1);int k=fact(i);
         	if(k<c) na(&i); printf("%d\n",k);exit(0);
 	}
 
-	if(strcmp(type,"long long\n")==0){
+	if(t=='l'){
         	long long c=i*(i-1);long long k=fact(i);
         	if(k<c) na(&i); printf("%lld\n",k);exit(0);
 	}
