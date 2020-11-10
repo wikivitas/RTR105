754100baed05c4028f40ddeb4fffe67ea066b2c3
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-11-10 15:33:12 +0200

    PU6_uzdevumu_izpilde

--- PU/PU6.c
+++ PU/PU6.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
-int main(){
+void first(){
+    printf("Sveiks tam, kurš nokompilēja un izpildīja!\n");   
+}
+void reiz(int a, int b){
+    size_t c=a*b;
+    printf("%d reiz %d ir %zu\n",a,b,c);
+}
+double pi(){
+    double pi=3.14159265358979;
+    return pi;
+}
+size_t fib(unsigned char n){
+    if (n == 1 || n == 2) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
 
-  return 0;
+int main()
+{
+    first();
+    reiz(146,213); //divu veselu skaitļu reizinājums
+    printf("PI = %.8lf\n",pi());
+    unsigned char n=36; //indeksa kārta Fibonači skaitlim
+    printf("%hhu. kārtas Fibonači skaitlis ir %zu",n,fib(n));
+    return 0;
 }
