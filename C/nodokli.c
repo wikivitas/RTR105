ed735bc2c0a6090a6273f7acc1654da845f1e5a1
wikivitas <71380540+wikivitas@users.noreply.github.com>
2020-11-19 00:46:19 +0200

    Patvaļīga kodu rakstīšana / Nodokļu kalkulātors

--- C/nodokli.c
+++ C/nodokli.c
@@ -5,24 +5,27 @@
 #define ATVAPG 250 //atvieglojums par 1 apgadāmo
 
 #define GNMMAX 3600 //maksimālais gada neapliekamais minimums
+#define GNMPEN 3600 //gada neapliekamais minimums pensionāram
 #define AIMIN 6000 //gada apliekamā ienākuma apmērs, līdz kuram piemēro maksimālo gada neapliekamo minimumu
 #define AIMAX 14400 //gada apliekamā ienākuma apmērs, virs kura nepiemēro diferencēto neapliekamo minimumu
 
 float tax_likme(float*, char*);
-float neap_min(float*, char*);
+float neap_min(float*, char*,char*);
 
 int main()
 {
     float alga; 
-    char n_gram; signed char apg;
+    char n_gram, pens; signed char apg;
     printf("Nodokļu kalkulātors\n-------------------\n");
     A:printf("Jūsu menēša bruto alga (EUR): ");scanf("%f",&alga);
     if(alga<=0) goto A;
     B:printf("Vai Jūms ir nodokļu gramatiņa? (1 - jā; 2 - nē): ");scanf(" %c",&n_gram);
     if(n_gram!='1' && n_gram!='2') goto B;
-    C:printf("Cik Jums ir apgadāmo skaits: ");scanf("%hhi",&apg);
-    if(apg<0) goto C;
-    float nm = neap_min(&alga,&n_gram);
+    C:printf("Cik Jums ir apgadāmo skaits(no 0 līdz 9): ");scanf("%hhi",&apg);
+    if(apg<0 || apg>9) goto C;
+    D:printf("Vai Jūs esat pensionārs? (1 - jā; 2 - nē): ");scanf(" %c",&pens);
+    if(pens!='1' && pens!='2') goto D;
+    float nm = neap_min(&alga,&n_gram,&pens);
     float tl = tax_likme(&alga,&n_gram);
     printf("Neapliekamais minimums: %.2f EUR\n",nm/12);
     float neto_alga = alga*(1-VSAOI)-tl*(alga*(1-VSAOI)-apg*ATVAPG-nm/12);
@@ -32,16 +35,20 @@ int main()
 
 float tax_likme(float* alga, char* n_gram){
     float k;
-    if(*alga<1667 && *n_gram=='Y') k=NL1;
-    if(*alga>=1667 || *n_gram=='N') k=NL2;
+    if(*alga<1667 && *n_gram=='1') k=NL1;
+    if(*alga>=1667 || *n_gram=='2') k=NL2;
     return k;
 }
 
-float neap_min(float* alga, char* n_gram){
-    if(*n_gram=='N'){ 
+float neap_min(float* alga, char* n_gram, char* pens){
+    if(*n_gram=='2' && *pens=='2'){ 
         return 0;
     }
-    if(*n_gram=='Y'){
+    if(*pens=='1'){ 
+        float gnm=GNMPEN;
+        return gnm;
+    }
+    if(*n_gram=='1' && *pens=='2'){
         float k=(float)GNMMAX/((float)AIMAX-(float)AIMIN);
         float gnm = (float)GNMMAX-k*((*alga)*12-(float)AIMIN);
         if(gnm>=(float)GNMMAX) return GNMMAX;
